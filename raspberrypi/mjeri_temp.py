d882ce3b42500570f09f5bff47f7075df82fd3ea
Martin Sven Kovačić <mskovacic10@gmail.com>
2016-05-13 20:50:48 +0200

    dorađena skrita za mjerenje temperature

--- raspberrypi/mjeri_temp.py
+++ raspberrypi/mjeri_temp.py
@@ -2,23 +2,32 @@
 
 import time
 import os
-import re
 
 filename = 'Podaci_o_temperaturi.txt'
+
+def get_temp():
+	os.system('temp > tmp')
+        with open('tmp', 'r') as f:
+                temp=float(f.readline().split("=")[1].split("'")[0])
+        os.remove('tmp')
+	return temp
+
+def write_temp(sum, ticks):
+	t=time.strftime("%c")
+        temp=str(sum/ticks)
+        with open(filename, 'a') as f:
+        	f.write("%s %s'C\n" % (t,temp))
+	return (0,0)
+
+
 sum=0
 ticks=0
 while True:
-	os.system('temp > tmp')
-	with open('temp', 'r') as f:
-		re.search
-		sum+=float(f.readline().split('=\'')[1])
+	sum+=get_temp()
 	ticks+=1
-	time.sleep(30)
+	time.sleep(1)
 	if ticks >= 10:
-		t=time.strftime("%c")
-		temp=str(sum/ticks)
-		with open(filename, 'a') as f:
-			f.write("%s %s\n" % (t,temp))
-		ticks=0
-		sum=0
-		#print ("%s %s" % (t,temp))
+		(sum, ticks)=write_temp(sum, ticks)
+		
+		
+
