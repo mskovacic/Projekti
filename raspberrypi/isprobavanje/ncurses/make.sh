9e36a831ddf528db47f47af15fe00cd846362acf
Martin Sven Kovačić <mskovacic10@gmail.com>
2016-05-20 21:17:29 +0200

    ispravljen regex

--- raspberrypi/isprobavanje/ncurses/make.sh
+++ raspberrypi/isprobavanje/ncurses/make.sh
@@ -1,6 +1,6 @@
 for i in *.c;
 do
     #echo "compiling $i";
-    output_name=`echo $i | sed -r "s/[a-zA-Z]*([0-9]+)\.c/proba\1.o/"`;
+    output_name=`echo $i | sed -r "s/\.c/.o/"`;
     gcc -lmenu -lform -lncurses -lpanel $i -o $output_name;
 done
