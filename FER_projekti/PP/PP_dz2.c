0239742de0df31a7931c5cf6b024c2667723a7d4
Martin Sven Kovačić <mskovacic10@gmail.com>
2016-05-23 19:10:49 +0200

    napisana funkcija za crtanje ploče

--- FER_projekti/PP/PP_dz2.c
+++ FER_projekti/PP/PP_dz2.c
@@ -5,18 +5,20 @@
 #include <time.h>
 #include <unistd.h>
 
-#define ARRAY_SIZE(a) (sizeof(a) / sizeof(a[0]))
+#define VISINA 6
+#define SIRINA 7
 #define BUFFER_SIZE 10
+#define GORNJI_RED 0
+#define SREDNJI_RED 1
+#define DONJI_RED 2
 #define POTEZ_CPU 0
 #define POTEZ_HUMAN 1
-#define COLOR_CPU 3
-#define COLOR_HUMAN 4
-#define COLOR_SELECT 5
-#define CTRLD 	4
 
 
 void Inicijaliziraj_MPI(int *argc, char ***argv, int *mpi_rank, int *mpi_size, char *processor_name);
 void Validiraj_MPI(int code);
+void Nacrtaj_plocu(char *ploca);
+void Nacrtaj_rub(char redak);
 int Iprobe(MPI_Status *status);
 void Recv(void *buf, MPI_Status *status);
 void Send(const void *buf, int dest);
@@ -26,22 +28,30 @@ void Wait(MPI_Request *request, MPI_Status *status);
 
 int main(int argc, char **argv) {
     int i, j, mpi_rank, mpi_size, igra_traje=1, potez;
-    char processor_name[MPI_MAX_PROCESSOR_NAME], string[1000];
+    char processor_name[MPI_MAX_PROCESSOR_NAME], *ploca, *broj_vrijednosti;
 
     Inicijaliziraj_MPI(&argc, &argv, &mpi_rank, &mpi_size, processor_name);
+	ploca = (char *) calloc(VISINA*SIRINA, sizeof(char));
+	broj_vrijednosti = (char *) calloc(SIRINA, sizeof(char));
+	memset(ploca, 46, VISINA*SIRINA);
+	memset(broj_vrijednosti, 48, SIRINA);
+	
+	
 	if (mpi_rank == 0) {
-
 		potez=POTEZ_HUMAN;
 		while (igra_traje) {
 			igra_traje=0;
+			i=system("clear");
 			switch (potez) {
 				case POTEZ_CPU:
-					//printf("CPU na potezu\n");
+					printf("CPU na potezu\n");
 					//TODO: DODATI LOGIKU OVDJE
+					
 					potez = POTEZ_HUMAN;
 					break;
 				case POTEZ_HUMAN:
-					//printf("Igracev potez!\nKoristi strelice za odabir stupca");
+					printf("Igračev potez!\n");
+					Nacrtaj_plocu(ploca);
 					//TODO: DODATI LOGIKU OVDJE
 				
 					potez = POTEZ_CPU;
@@ -53,7 +63,7 @@ int main(int argc, char **argv) {
 			
 		}
 		
-		printf("Igra je završena!\nPritisnite neku tipku za izlazak...");
+		printf("Igra je završena!\nPritisnite neku tipku za izlazak...\n");
 	}
 	Validiraj_MPI(MPI_Finalize());
     return 0;
@@ -79,6 +89,40 @@ void Validiraj_MPI(int code) {
 	    MPI_Error_string(code, error_string, &resultlen);
 	    perror(error_string);
     }
+}
+
+void Nacrtaj_plocu(char *ploca) {
+	int i,j;
+	
+	Nacrtaj_rub(GORNJI_RED);
+	for (i=0; i<VISINA; i++) {
+		printf ("%s", "│");
+		for (j=0; j<SIRINA; j++) {
+			printf (" %c %s", ploca[j+i*SIRINA], "│");
+		}
+		printf ("\n");
+		if (i!= VISINA-1) Nacrtaj_rub(SREDNJI_RED);
+	}
+	Nacrtaj_rub(DONJI_RED);
+}
+
+void Nacrtaj_rub(char redak) {
+	char red_element[10];
+	
+	sprintf(red_element, "%s", "───");
+	switch (redak) {
+		case GORNJI_RED:
+			printf ("%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s\n", "┌", red_element, "┬", red_element, "┬", red_element, "┬", red_element, "┬", red_element, "┬", red_element, "┬", red_element, "┐");
+			break;
+		case SREDNJI_RED:
+			printf ("%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s\n", "├", red_element, "┼", red_element, "┼", red_element, "┼", red_element, "┼", red_element, "┼", red_element, "┼", red_element, "┤");
+			break;
+		case DONJI_RED:
+			printf ("%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s\n", "└", red_element, "┴", red_element, "┴", red_element, "┴", red_element, "┴", red_element, "┴", red_element, "┴", red_element, "┘");
+			break;
+		default:
+			perror("Nacrtaj_rub error");
+	}
 }
 
 int Iprobe(MPI_Status *status) {
