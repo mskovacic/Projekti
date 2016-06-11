08404d127007527a1c3e7a6c8b86d24929459c99
Martin Sven Kovačić <mskovacic10@gmail.com>
2016-06-11 18:48:00 +0200

    crtanje u datoteku

--- raspberrypi/isprobavanje/cvx/example_1.py
+++ raspberrypi/isprobavanje/cvx/example_1.py
@@ -1,55 +1,75 @@
 #Total variation in-painting
 #Grayscale Images
+#http://nbviewer.jupyter.org/github/cvxgrp/cvxpy/blob/master/examples/notebooks/WWW/tv_inpainting.ipynb
 
 from PIL import Image
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import numpy as np
+from cvxpy import *
+
+def get_greyscale((r,g,b,a)):
+	return (r+g+b)/3
 
 # Load the images.
-orig_img = Image.open("data/lena512.png")
-corr_img = Image.open("data/lena512_corrupted.png")
-
+orig_img = Image.open("lena.png")
+corr_img = Image.open("lena_corrupted.png")
 
 # Convert to arrays.
 Uorig = np.array(orig_img)
 Ucorr = np.array(corr_img)
-rows, cols = Uorig.shape
+rows, cols, tmp= Uorig.shape
 
 # Known is 1 if the pixel is known,
 # 0 if the pixel was corrupted.
 Known = np.zeros((rows, cols))
+Ucorrupted = []
+Uoriginal = []
 for i in xrange(rows):
-    for j in xrange(cols):
-         if Uorig[i, j] == Ucorr[i, j]:
-            Known[i, j] = 1
+	row_corrupted = []
+	row_original = []
+	for j in xrange(cols):
+		#print Uorig[i, j], Ucorr[i, j]
+		if Uorig[i, j].all() == Ucorr[i, j].all():
+			Known[i, j] = 1
+		row_corrupted.append(get_greyscale(Ucorr[i, j]))
+		row_original.append(get_greyscale(Uorig[i, j]))
+	Ucorrupted.append(row_corrupted)
+	Uoriginal.append(row_original)
 
-%matplotlib inline
-fig, ax = plt.subplots(1, 2,figsize=(10, 5))
-ax[0].imshow(orig_img);
-ax[0].set_title("Original Image")
-ax[0].axis('off')
-ax[1].imshow(corr_img);
-ax[1].set_title("Corrupted Image")
-ax[1].axis('off');
+#%matplotlib inline
+#fig, ax = plt.subplots(1, 2,figsize=(10, 5))
+#ax[0].imshow(orig_img);
+#ax[0].set_title("Original Image")
+#ax[0].axis('off')
+#ax[1].imshow(corr_img);
+#ax[1].set_title("Corrupted Image")
+#ax[1].axis('off');
 
 # Recover the original image using total variation in-painting.
-from cvxpy import *
-U = Variable(rows, cols)
+U = Variable(rows, cols, 4)
 obj = Minimize(tv(U))
 constraints = [mul_elemwise(Known, U) == mul_elemwise(Known, Ucorr)]
 prob = Problem(obj, constraints)
 # Use SCS to solve the problem.
 prob.solve(verbose=True, solver=SCS)
 
-fig, ax = plt.subplots(1, 2,figsize=(10, 5))
+#fig, ax = plt.subplots(1, 2,figsize=(10, 5))
 # Display the in-painted image.
 img_rec = Image.fromarray(U.value)
-ax[0].imshow(img_rec);
-ax[0].set_title("In-Painted Image")
-ax[0].axis('off')
+#ax[0].imshow(img_rec);
+#ax[0].set_title("In-Painted Image")
+#ax[0].axis('off')
 
-img_diff = Image.fromarray(10*np.abs(Uorig - U.value))
-ax[1].imshow(img_diff);
-ax[1].set_title("Difference Image")
-ax[1].axis('off');
+img_diff = Image.fromarray(10*np.abs(Uoriginal - U.value))
+#ax[1].imshow(img_diff);
+#ax[1].set_title("Difference Image")
+#ax[1].axis('off');
 
+box = orig_img.getbbox()
+image = Image.new("RGB", (box[2], box[3]))
+image.paste(img_rec, (0,0))
+image.save("lena_recovered.png")
+
+image = Image.new("RGB", (box[2], box[3]))
+image.paste(img_diff, (0,0))
+image.save("lena_d.png")
--- raspberrypi/isprobavanje/cvx/example_2.py
+++ raspberrypi/isprobavanje/cvx/example_2.py
@@ -1,13 +1,21 @@
 #Total variation in-painting
 #Color Images
+#http://nbviewer.jupyter.org/github/cvxgrp/cvxpy/blob/master/examples/notebooks/WWW/tv_inpainting.ipynb
+
+def display_image(image, name, heigth, width):
+	image = Image.new("RGB", (heigth, width))
+	image.paste(image, (0,0))
+	image.save(name)
 
 from PIL import Image
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import numpy as np
 
 np.random.seed(1)
 # Load the images.
-orig_img = Image.open("data/lena512color.tiff")
+orig_img = Image.open("lena_color.png")
+box = orig_img.getbbox()
+display_image(orig_img, "lena_original.png", box[2], box[3])
 
 # Convert to arrays.
 Uorig = np.array(orig_img)
@@ -25,16 +33,17 @@ for i in xrange(rows):
             
 Ucorr = Known*Uorig
 corr_img = Image.fromarray(np.uint8(Ucorr))
+display_image(corr_img, "lena_corrupted_2.png", box[2], box[3])
 
 # Display the images.
-%matplotlib inline
-fig, ax = plt.subplots(1, 2,figsize=(10, 5))
-ax[0].imshow(orig_img);
-ax[0].set_title("Original Image")
-ax[0].axis('off')
-ax[1].imshow(corr_img);
-ax[1].set_title("Corrupted Image")
-ax[1].axis('off');
+#%matplotlib inline
+#fig, ax = plt.subplots(1, 2,figsize=(10, 5))
+#ax[0].imshow(orig_img);
+#ax[0].set_title("Original Image")
+#ax[0].axis('off')
+#ax[1].imshow(corr_img);
+#ax[1].set_title("Corrupted Image")
+#ax[1].axis('off');
 
 # Recover the original image using total variation in-painting.
 from cvxpy import *
@@ -48,23 +57,26 @@ for i in xrange(colors):
 prob = Problem(Minimize(tv(*variables)), constraints)
 prob.solve(verbose=True, solver=SCS)
 
-import matplotlib.pyplot as plt
-import matplotlib.cm as cm
-%matplotlib inline
+#import matplotlib.pyplot as plt
+#import matplotlib.cm as cm
+#%matplotlib inline
 
 # Load variable values into a single array.
 rec_arr = np.zeros((rows, cols, colors), dtype=np.uint8)
 for i in xrange(colors):
     rec_arr[:, :, i] = variables[i].value
 
-fig, ax = plt.subplots(1, 2,figsize=(10, 5))
+#fig, ax = plt.subplots(1, 2,figsize=(10, 5))
 # Display the in-painted image.
 img_rec = Image.fromarray(rec_arr)
-ax[0].imshow(img_rec);
-ax[0].set_title("In-Painted Image")
-ax[0].axis('off')
+#ax[0].imshow(img_rec);
+#ax[0].set_title("In-Painted Image")
+#ax[0].axis('off')
 
 img_diff = Image.fromarray(np.abs(Uorig - rec_arr))
-ax[1].imshow(img_diff);
-ax[1].set_title("Difference Image")
-ax[1].axis('off');
+#ax[1].imshow(img_diff);
+#ax[1].set_title("Difference Image")
+#ax[1].axis('off');
+
+display_image(img_rec, "lena_recovered.png", box[2], box[3])
+display_image(img_diff, "lena_d.png", box[2], box[3])
